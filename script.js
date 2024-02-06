1f8fc348c0c63374ba3af9841190ff4a19777da0
luis Colina <colina.1990lz@gmail.com>
2024-02-06 06:22:40 -0400

    animations and thank you state added

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const form = document.querySelector("#form")
+const thankyou = document.querySelector("#thankbtn")
+const cardNumber = document.querySelector("#card_number")
+const cardMonth = document.querySelector("#card_month")
+const cardYear = document.querySelector("#card_year")
+const cardName = document.querySelector("#card_name")
+const cardCVC = document.querySelector(".back__number")
 
 form.addEventListener("submit", function(e){
     e.preventDefault()
@@ -12,17 +18,27 @@ form.addEventListener("submit", function(e){
     const cleanedValue = number.replace(/\s/g, '').substring(0, 16);
     const formattedValue = cleanedValue.replace(/(.{4})/g, '$1 ').trim();
 
-    const cardNumber = document.querySelector("#card_number")
-    const cardMonth = document.querySelector("#card_month")
-    const cardYear = document.querySelector("#card_year")
-    const cardName = document.querySelector("#card_name")
-    const cardCVC = document.querySelector(".back__number")
+    toggleFadeInCards()
 
-    cardNumber.textContent = formattedValue
-    cardMonth.textContent = date
-    cardYear.textContent = year
-    cardCVC.textContent = cvc
-    cardName.textContent = name
+    setTimeout(function(){
+        cardNumber.textContent = formattedValue
+        cardMonth.textContent = date
+        cardYear.textContent = year
+        cardCVC.textContent = cvc
+        cardName.textContent = name
+        toggleFadeInCards()
+    }, 1000)
+
+    thankYouState()
+})
+
+thankyou.addEventListener("click" , function(){
+    document.querySelector("#name").value = ""
+    document.querySelector("#number").value = ""
+    document.querySelector("#date").value = ""
+    document.querySelector("#year").value = ""
+    document.querySelector("#cvc").value = ""
+    thankYouState()
 })
 
 function addZero(str){
@@ -31,3 +47,23 @@ function addZero(str){
     }
     return str
 }
+function toggleFadeInCards(){
+
+    let arr = [cardNumber, cardMonth, cardYear, cardName, cardCVC]
+    arr.map(e => e.classList.toggle("hide"))
+}
+
+function thankYouState(){
+
+    let form = document.querySelector("#form")
+    let thankYou = document.querySelector(".completed_state")
+    let arr = [form, thankYou]
+    const animation = () => arr.map(e => e.classList.toggle("hide"))
+    animation()
+    setTimeout(function(){
+        arr.map(e => e.classList.toggle("none"))
+        animation()
+    }, 1000)  
+
+}
+
